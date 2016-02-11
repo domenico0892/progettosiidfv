74b4657429d3ffb1b405d9deea6cee9b01b00490
k0ny <vincenzo.giordano.kony@gmail.com>
2016-02-11 16:00:14 +0100

    FRANCEEEE

--- src/main/java/it/uniroma3/model/MyCrawler.java
+++ src/main/java/it/uniroma3/model/MyCrawler.java
@@ -1,18 +1,25 @@
 package it.uniroma3.model;
 
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import org.bson.Document;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -47,29 +54,31 @@ public class MyCrawler extends WebCrawler {
 			reader = new FileReader("config.json");
 	         JSONParser jsonParser = new JSONParser();		 
 			JSONObject cj = (JSONObject) jsonParser.parse(reader);
-			/*Capabilities caps = new DesiredCapabilities();
+			Capabilities caps = new DesiredCapabilities();
 			((DesiredCapabilities) caps).setJavascriptEnabled(true);                
 			((DesiredCapabilities) caps).setCapability("takesScreenshot", true); 
-			((DesiredCapabilities) caps).setCapability("webSecurityEnabled", false);
+			//((DesiredCapabilities) caps).setCapability("webSecurityEnabled", false);
 			((DesiredCapabilities) caps).setCapability(
 					PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
 					cj.get("phantomjs")
-					);*/
-			ArrayList<String> cliArgsCap = new ArrayList<String>();
-			DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
-			cliArgsCap.add("--web-security=false");
+					);
+			//ArrayList<String> cliArgsCap = new ArrayList<String>();
+			//DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
+			/*cliArgsCap.add("--web-security=false");
 			cliArgsCap.add("--ssl-protocol=any");
 			cliArgsCap.add("--ignore-ssl-errors=true");
-			capabilities.setCapability("takesScreenshot", true);
-			capabilities.setCapability(
-			    PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
-			capabilities.setCapability(
-			    PhantomJSDriverService.PHANTOMJS_GHOSTDRIVER_CLI_ARGS,
-			        new String[] { "--logLevel=2" });
-			capabilities.setCapability(
-					PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-					cj.get("phantomjs"));
-			this.driver = new PhantomJSDriver(capabilities);
+			*/
+//			capabilities.setCapability("takesScreenshot", true);
+//			capabilities.setCapability(
+//			    PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
+//			capabilities.setCapability(
+//			    PhantomJSDriverService.PHANTOMJS_GHOSTDRIVER_CLI_ARGS,
+//			        new String[] { "--logLevel=2" });
+//			capabilities.setCapability(
+//					PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+//					cj.get("phantomjs"));
+			//this.driver = new PhantomJSDriver(capabilities);
+			this.driver = new PhantomJSDriver(caps);
 			
 		} catch (IOException | ParseException e) {
 			// TODO Auto-generated catch block
@@ -119,20 +128,49 @@ public class MyCrawler extends WebCrawler {
 			//HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
 			//String html = htmlParseData.getHtml();
 			this.driver.get(url);
-			URL url_parsed;
-			try {
-				System.out.println("salvataggio");
-				url_parsed = new URL (url);
-				Document doc = new Document();
-				doc.append("url", url);
-				doc.append("host", url_parsed.getHost());
-				doc.append("html", this.driver.getPageSource());
-				this.coll.insertOne(doc);
-				System.out.println(url + "salvato");
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+//			WebElement iframe = driver.findElement(By.tagName("iframe"));
+//			driver.switchTo().frame(iframe);
+//			WebElement iframeElement = driver.findElement(By.id("dsq-app2"));
+			//driver.switchTo().frame(iframeElement);
+//			System.out.println(driver.getPageSource());
+			String pageS = this.driver.getPageSource();
+			List<WebElement> iframeElements = driver.findElements(By.tagName("iframe"));
+			for(WebElement we : iframeElements){
+				driver.switchTo().frame(we);
+				String iframeS = this.driver.getPageSource();
+				pageS = manipolareHtml(pageS, iframeS);
+				//System.out.println("Iframe 	WURT************************************ \n \n" + this.driver.getPageSource() + "\n *************************************************");
+				driver.switchTo().defaultContent();
 			}
+			
+//			
+//			URL url_parsed;
+//			try {
+//				System.out.println("salvataggio");
+//				url_parsed = new URL (url);
+//				Document doc = new Document();
+//				doc.append("url", url);
+//				doc.append("host", url_parsed.getHost());
+//				//doc.append("html", this.driver.getPageSource());
+//				doc.append("html", pageS);
+//				this.coll.insertOne(doc);
+//				System.out.println(url + "salvato");
+//			} catch (MalformedURLException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
 		}
+	}
+
+
+	private String manipolareHtml(String pageS, String iframeS) {
+		org.jsoup.nodes.Document doc = Jsoup.parse(pageS);
+		Element iframe = doc.select("iframe").first();
+		iframe.after(iframeS);
+		iframe.remove();
+//		doc.select("iframe").first().after(iframeS);
+//		doc.select("iframe").first().remove();
+		//return doc.text();
+		return doc.text();
 	}
 }
